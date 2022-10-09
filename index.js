f6efd2686f1aeee0e04adf11a5a7eb40e7b7f5d2
Ankit <ankit.95sharma@gmail.com>
2022-10-10 03:19:36 +0530

    Revert "feat: output table added"
    This reverts commit cb39da415cf972987b041d9b1d25dddae189ec92.

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const checkButton = document.querySelector("#check-button");
 const nextButton = document.querySelector("#next-button");
 const cashGivenField = document.querySelector(".cash-given-field");
 const message = document.querySelector(".errorMsg");
-const num_change = document.querySelectorAll(".numChange");
-const deno = [1, 5, 10, 20, 100, 500, 2000];
+const outputContainer = document.querySelector(".output");
+const numChange = document.querySelectorAll(".numChange");
+
+const denominations = [1, 5, 10, 20, 100, 500, 2000];
 
 function showMessage(msg) {
   message.style.display = "block";
@@ -17,16 +19,20 @@ function hideMessage() {
 }
 
 function validateInputs() {
+  let billValue = Number(billAmount.value);
+  let cashValue = Number(cashGivenInput.value);
   hideMessage();
-  if (billAmount.value > 0 && cashGivenInput.value > 0) {
-    if (cashGivenInput.value < billAmount.value) {
-      showMessage("Cash amount is less than the Bill, Please enter correct value");
-    }
-    else if (billAmount.value === cashGivenInput.value) {
+  if (billValue > 0 && cashValue > 0) {
+    console.log(typeof billValue, cashValue);
+    if (cashValue < billValue) {
+      showMessage(
+        "Cash amount is less than the Bill, Please enter correct value"
+      );
+    } else if (billValue === cashValue) {
       showMessage("No amount should be returned");
-    }
-    else{
-      return_change(cashGivenInput.value - billAmount.value);
+    } else {
+      returnChange(cashValue - billValue);
+      outputContainer.style.display = "block";
     }
   } else {
     showMessage("Invalid Bill Amount or Cash Given value!");
@@ -34,7 +40,9 @@ function validateInputs() {
 }
 
 nextButton.addEventListener("click", () => {
-  if (billAmount.value > 0) {
+  let billValue = Number(billAmount.value);
+  hideMessage();
+  if (billValue > 0) {
     cashGivenField.style.display = "block";
     nextButton.style.display = "none";
   } else {
@@ -44,12 +52,10 @@ nextButton.addEventListener("click", () => {
 
 checkButton.addEventListener("click", validateInputs);
 
-
-function return_change(balance_amount) {
-
-  for (let i = deno.length - 1; i > -1; i--) {
-    let num_deno = Math.trunc(balance_amount / deno[i]);
-    balance_amount %= deno[i];
-    num_change[i].innerText = num_deno;
+function returnChange(balanceAmount) {
+  for (let i = denominations.length - 1; i > -1; i--) {
+    let numberOfDenominations = Math.trunc(balanceAmount / denominations[i]);
+    balanceAmount %= denominations[i];
+    numChange[i].innerText = numberOfDenominations;
   }
 }
